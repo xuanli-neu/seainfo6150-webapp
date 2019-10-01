d1fb31a932d8c15a508d77271894e73f44cfb3ee
xuanli-neu <55112305+xuanli-neu@users.noreply.github.com>
2019-10-01 16:30:24 -0700

    Update Article.jsx

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -1,13 +1,8 @@
-      import React from 'react'
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>Assignment 3</title>
-      </head>
-      <body>
+import React from 'react'
+
+const Article = () => {
+  return (
+    <div>
       
       <article>      
       <h1>The Statue of Liberty's torch heads to new museum</h1>  
@@ -58,5 +53,8 @@
         <li>Total weight of the Statue of Liberty is <b>225 tons (or 450,000 pounds)</b></li>
         </ul>
    </article>
-   </body>
-   </html>
+  </div>
+  )
+}
+
+export default Article
