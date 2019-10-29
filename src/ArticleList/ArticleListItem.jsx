b70df671a300b4e74c08ed17b22292664902efb2
xuanli-neu <li.xuan2@husky.neu.edu>
2019-10-29 13:24:43 -0700

    Assignment 8

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -4,18 +4,34 @@ import SlugButton from './SlugButton.jsx'
 import ArticleImage from './ArticleImage.jsx'
 import './ArticleListItem.module.css'
 
-const ArticleListItem = props =>{
-	return (
-		<div>
-     	<ArticleImage article = {props.article}/>
-		<strong><b>{props.article.title}</b></strong>
-        <p>{props.article.shortText}</p>
-        <b><time dateTime = {props.article.pubYear}>{props.article.pubDate}</time></b>
-        <SlugButton article = {props.article}/>                                        
-        </div> 
-    );
+const ArticleListItem = props =>(
 
-};
+    <div>
+        {/* <!DOCTYPE html> */}
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <title>Week6 assignment</title>
+            </head>
+
+            <body >
+                <ArticleImage article = {props.article}/>
+                <SlugButton article = {props.article}/>
+                <section id = "container">
+                    <h1 class = "item-h1">{props.article.title}</h1> 
+                    <p class = "item-p">{props.article.shortText}</p>
+                    <time class = "item -time" dateTime = {props.article.pubDate}>{props.article.pubDate}</time>
+                    
+                </section>
+                
+            </body>
+            
+        </html>
+    </div>
+
+);
 
 
 ArticleListItem.propTypes = {
